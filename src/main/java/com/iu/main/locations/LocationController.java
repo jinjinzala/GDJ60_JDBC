98a899cc8d25d8166e8839760e3a86ef255d8512
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-24 21:46:45 +0900

    Update LocationController.java

--- src/main/java/com/iu/main/locations/LocationController.java
+++ src/main/java/com/iu/main/locations/LocationController.java
@@ -3,64 +3,81 @@ package com.iu.main.locations;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import com.iu.main.departments.DepartmentController;
-import com.iu.main.departments.DepartmentDAO;
-import com.iu.main.departments.DepartmentDTO;
-
 public class LocationController {
-
 	private Scanner sc;
 	private LocationDAO locationDAO;
+	private LocationView locationView;
 	private LocationInput locationInput;
-	private LocationDTO locationDTO;
 	
 	public LocationController() {
 		this.sc = new Scanner(System.in);
 		this.locationDAO = new LocationDAO();
+		this.locationView = new LocationView();
 		this.locationInput = new LocationInput();
 	}
+	
 	public void start() throws Exception {
 		boolean check = true;
+		ArrayList<LocationDTO> ar = null;
+		LocationDTO locationDTO = null;;
 		
-		ArrayList<LocationDTO> ar =null;
 		while(check) {
-			System.out.println("1.부서 리스트");	
-			System.out.println("2.부서 상세 정보");
-			System.out.println("3.주 소 검 색");
-			System.out.println("4.업데이트 데이터 ");
-			System.out.println("5.인서트 데이터 ");
-			System.out.println("6.종    료");
+			System.out.println("1.주소 리스트");
+			System.out.println("2.주소 상세정보");
+			System.out.println("3.주소 검색");
+			System.out.println("4.주소 추가");
+			System.out.println("5.주소 삭제");
+			System.out.println("6.주소 수정");
+			System.out.println("7.종료");
 			int select = sc.nextInt();
-			switch (select) {
+			switch(select) {
 			case 1:
-				 ar = locationDAO.getList();
+				ar = locationDAO.getList();
+				locationView.view(ar);
 				break;
 			case 2:
-				System.out.println("로케이션 넘버를 입력하세요");
+				System.out.print("location 번호를 입력하세요: ");
 				select = sc.nextInt();
-		        locationDAO.getDetail(select);
-				break;
-			case 3:
-				System.out.println("검색할 주소를 입력 ");
-				String add = sc.next();
-			    ar = locationDAO.getFind(add);
-				break;
-			case 4:	
-				//업데이트 데이터 
-			   locationDTO = locationInput.updateData();
-			   select = locationDAO.updateData(locationDTO);
-			   if(select > 0) {
-				   System.out.println("수정이 성공 ");
+				locationDTO = locationDAO.getDetail(select);
+				if(locationDTO != null) {
+					locationView.view(locationDTO);
 				}else {
-					 System.out.println("수정이 실패 ");
+					locationView.view("Data가 없습니다.");
 				}
 				break;
-				
-//			case 5:	
-//				//인서트 데이터 
-//				
-//				break;
-			
+			case 3:
+				System.out.print("검색할 주소 입력: ");
+				String add = sc.next();
+				ar = locationDAO.getFind(add);
+				locationView.view(ar);
+				break;
+			case 4:
+				locationDTO = locationInput.setData();
+				select = locationDAO.setData(locationDTO);
+				String msg = "추가 실패";
+				if(select>0) {
+					msg = "추가 성공";
+				}
+				locationView.view(msg);
+				break;
+			case 5:
+				locationDTO = locationInput.deleteData();
+				select = locationDAO.deleteData(locationDTO);
+				if(select > 0) {
+					locationView.view("삭제 성공");
+				}else {
+					locationView.view("삭제 실패");				
+				}
+				break;
+			case 6:
+				locationDTO = locationInput.updateData();
+				select = locationDAO.updateDate(locationDTO);
+				if(select > 0) {
+					locationView.view("수정 성공");
+				}else {
+					locationView.view("수정 실패");				
+				}
+				break;
 			default:
 				check = false;
 			}
