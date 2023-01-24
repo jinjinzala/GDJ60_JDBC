ebf929052aaf66a07e1b2163c36df1961a880afe
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-24 23:14:43 +0900

    Update EmployeesDTO.java

--- src/main/java/com/iu/main/employees/EmployeesDTO.java
+++ src/main/java/com/iu/main/employees/EmployeesDTO.java
@@ -3,30 +3,19 @@ package com.iu.main.employees;
 import java.sql.Date;
 
 public class EmployeesDTO {
-    //DTO만드는 규칙
-	//모든 멤버 변수의 접근 지정자는 private
-	//Table의 칼럼의 데이터타입과 컬럼명과 일치 시킴 
-	//Getter Setter 생성
-	//생성자는 여러개 있어도 상관 x
-	//꼭 기본 생성자(디폴트 생성자를 자동으로 만들어줌)는 있어야 함 
-	
-	//null값이 들어올 가능성이 있기때문에 integer(레퍼런스타입) 사용 
-	//변수명의 첫번째는 소문자로 시작하기 때문에 소문자로 작성 (컬럼명은 대소문자 구분 x)
-	
-	//사원정보리스트 
-	
+
 	private Integer employee_id;
 	private String first_name;
 	private String last_name;
-	private String email;
+	private String eamil;
 	private String phone_number;
 	private String hire_date;
-	private String salary;
-	//commission_pct는 double로 선언하는게 좋다.
-	private Integer commission_pct;
+	private String job_id;
+	private double salary;
+	private double commision_pct;
 	private Integer manager_id;
 	private Integer department_id;
-	private String job_id;
+	
 	
 	
 	public Integer getEmployee_id() {
@@ -47,11 +36,11 @@ public class EmployeesDTO {
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
 	}
-	public String getEmail() {
-		return email;
+	public String getEamil() {
+		return eamil;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+	public void setEamil(String eamil) {
+		this.eamil = eamil;
 	}
 	public String getPhone_number() {
 		return phone_number;
@@ -65,17 +54,23 @@ public class EmployeesDTO {
 	public void setHire_date(String hire_date) {
 		this.hire_date = hire_date;
 	}
-	public String getSalary() {
+	public String getJob_id() {
+		return job_id;
+	}
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+	public double getSalary() {
 		return salary;
 	}
-	public void setSalary(String salary) {
+	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-	public Integer getCommission_pct() {
-		return commission_pct;
+	public double getCommision_pct() {
+		return commision_pct;
 	}
-	public void setCommission_pct(Integer commission_pct) {
-		this.commission_pct = commission_pct;
+	public void setCommision_pct(double commision_pct) {
+		this.commision_pct = commision_pct;
 	}
 	public Integer getManager_id() {
 		return manager_id;
@@ -89,14 +84,5 @@ public class EmployeesDTO {
 	public void setDepartment_id(Integer department_id) {
 		this.department_id = department_id;
 	}
-	public String getJob_id() {
-		return job_id;
-	}
-	public void setJob_id(String job_id) {
-		this.job_id = job_id;
-	}
+	
 }
-	
-	
-	
-	
