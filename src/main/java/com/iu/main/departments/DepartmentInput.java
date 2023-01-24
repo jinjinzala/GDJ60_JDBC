ee6c19152e05aad38750178c324404131aa0c6e7
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-24 20:12:46 +0900

    Update DepartmentInput.java

--- src/main/java/com/iu/main/departments/DepartmentInput.java
+++ src/main/java/com/iu/main/departments/DepartmentInput.java
@@ -4,46 +4,42 @@ import java.util.Scanner;
 
 public class DepartmentInput {
 	private Scanner sc;
-	//추가할때 받아야하는 메서드
-	public DepartmentInput(){
-	sc = new Scanner(System.in);
+	
+	public DepartmentInput() {
+		this.sc = new Scanner(System.in);
 	}
 	
-	public DepartmentDTO updateData() {
+	//추가
+	public DepartmentDTO setDate() {
 		DepartmentDTO departmentDTO = new DepartmentDTO();
-		System.out.println("수정할 부서 번호 입력");
-	    departmentDTO.setDepartment_id(sc.nextInt());
-	    System.out.println("수정할 부서명 입력");
-	    departmentDTO.setDepartment_name(sc.next());
-	   //매니저 번호 지역번호
-	    
-	    return departmentDTO;
-	    
+		System.out.print("부서명 입력: ");
+		departmentDTO.setDepartment_name(sc.next());
+		System.out.print("매니저 번호 입력: ");
+		departmentDTO.setManager_id(sc.nextInt());
+		System.out.print("지역 번호 입력: ");
+		departmentDTO.setLocation_id(sc.nextInt());
+		return departmentDTO;
 	}
-	
-	
-	public DepartmentDTO setData() {
-	DepartmentDTO departmentDTO = new DepartmentDTO();	
-	System.out.println("부서명 입력 ");	
-	departmentDTO.setDepartment_name(sc.next());
-	System.out.println("매니저 번호 입력 ");
-	departmentDTO.setManager_id(sc.nextInt());
-	System.out.println("지역 번호 입력 ");
-	departmentDTO.setLocation_id(sc.nextInt());
-	
-	return departmentDTO;
-	
-	}	
-	
-	//삭제할때 받아야하는 메서드 
-	
-	public DepartmentDTO deleteData() {
-		DepartmentDTO departmentDTO =new DepartmentDTO();
-		System.out.println("삭제할 부서 번호 입력");
+
+	//삭제
+	public DepartmentDTO deleteDate() {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		System.out.print("부서 번호 입력: ");
 		departmentDTO.setDepartment_id(sc.nextInt());
 		return departmentDTO;
 	}
 	
-	
-	
+	//수정
+	public DepartmentDTO updateDate() {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		System.out.print("수정할 부서 번호 입력: ");
+		departmentDTO.setDepartment_id(sc.nextInt());
+		System.out.print("수정할 부서 명 입력: ");
+		departmentDTO.setDepartment_name(sc.next());
+		System.out.print("수정할 매니저 번호 입력: ");
+		departmentDTO.setManager_id(sc.nextInt());
+		System.out.print("수정할 지역 번호 입력: ");
+		departmentDTO.setLocation_id(sc.nextInt());
+		return departmentDTO;
+	}
 }
