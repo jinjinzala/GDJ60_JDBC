5d4df39f0aa67d39646d64febdd3823de598128a
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-24 23:31:01 +0900

    Update EmployeesController.java

--- src/main/java/com/iu/main/employees/EmployeesController.java
+++ src/main/java/com/iu/main/employees/EmployeesController.java
@@ -1,86 +1,93 @@
-
-package com.iu.main.departments;
+package com.iu.main.employees;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import org.springframework.core.io.FileSystemResource;
+public class EmployeesController {
+//사원정보리스트는 신입사원순으로 출력
+//사원검색은 사원이름으로 검색
 
-public class DepartmentController {
-    private Scanner sc;
-    private DepartmentDAO departmentDAO;
-    private  DepartmentView departmentView;
-    private DepartmentInput departmentInput; 
 	
-    public DepartmentController() {
+	private Scanner sc;
+	private EmployeesDAO employeesDAO;
+	private EmployeesView employeesView;
+	public EmployeesController() {
 		this.sc = new Scanner(System.in);
-	    this.departmentDAO = new DepartmentDAO();
-	    this.departmentView = new DepartmentView();
-	    this.departmentInput = new DepartmentInput();
+	    this.employeesDAO = new EmployeesDAO();
+	    this.employeesView = new EmployeesView();
 	}
-    
-	public void start() throws Exception {
-		boolean check=true;
-		DepartmentDTO departmentDTO=null;
-		ArrayList<DepartmentDTO> ar=null;
+	public void start() throws Exception{
+		boolean check = true;
+		ArrayList<EmployeesDTO> ar =null;
+		EmployeesDTO employeesDTO = null;
 		while(check) {
-			System.out.println("1.부서 리스트");
-			System.out.println("2.부서 상세 정보");
-			System.out.println("3.부서 추가");
-			System.out.println("4.부서 삭제");
-			System.out.println("5.부서 수정");
-			System.out.println("6.종   료");
+			System.out.println("1.사원 전체 리스트");
+			System.out.println("2.사원 번호로 검색_개별사원정보출력 ");
+			System.out.println("3.사원 검색");
+            System.out.println("4.사원 추가");
+            System.out.println("5.사원 삭제");
+            System.out.println("6.사원 수정");
+            System.out.println("7.종    료");
+		//입력한 번호의 값을 받아오는 함수 	
 			int select = sc.nextInt();
-			
 			switch(select){
 			case 1:
-				 ar = departmentDAO.getList();
-				departmentView.view(ar);
+				ar = employeesDAO.getList();
+				employeesView.view(ar);			
 				break;
-			case 2:
-				System.out.println("부서번호를 입력하세요");
-				select = sc.nextInt();
-				departmentDTO = departmentDAO.getDetail(select);
-				if(departmentDTO != null) {
-					departmentView.view(departmentDTO);
-				}else {
-					departmentView.view("data가 없습니다");
-				}
-				break;
-			case 3: 
-				departmentDTO  = departmentInput.setDate();
-				select = departmentDAO.setData(departmentDTO);
-				
-				if(select>0) {
-					departmentView.view("추가 성공");
-				}else {
-					departmentView.view("추가 실패");
-				}
-				break;
-			case 4:
-				departmentDTO = departmentInput.deleteDate();
-				select = departmentDAO.deleteData(departmentDTO);
-				String msg = "삭제실패";
-				if(select>0) {
-					msg = "삭제 성공";
-					
-				}departmentView.view(msg);
-				break;
-				
-			case 5:	
-				departmentDTO = departmentInput.updateDate();
-				select = departmentDAO.updateData(departmentDTO);
-				if(select>0) {
-					departmentView.view("수정 성공");
-				}else {
-					departmentView.view("수정 실패");
-				}
-				break;
+		case 2:
+			System.out.println("검색할 사원번호를 입력해주세요");
+			select = sc.nextInt();
+			employeesDTO = employeesDAO.getDetail(select);
+			if(employeesDTO != null) {
+				employeesView.view(employeesDTO);
+			}else {
+				employeesView.view("data가 없습니다.");
+			}
+			break;
+//		case 3:
+//			System.out.print("검색할 사원 이름 입력:");
+//			String add = sc.next();
+//			ar = employeesDAO.getFind(add);
+//			employeesView.view(ar);
+//			break;
+//		case 4:
+//			employeesDTO = employeeInput.setData();
+//			select = employeesDAO.setData(employeesDTO);
+//			if(select > 0) {
+//				employeesView.view("추가에 성공하였습니다");
+//			}else {
+//				employeesView.view("추가에 실패하였습니다");
+//			}
+//			break;
+//		case 5:
+//			employeesDTO = employeeInput.deleteData();
+//			select = employeesDAO.deleteData(employeesDTO);
+//			if(select > 0) {
+//				employeesView.view("사원 삭제를 성공하였습니다");
+//			}else {
+//				employeesView.view("사원 삭제를 실패하였습니다");
+//			}
+//			break;
+//		case 6:
+//			employeesDTO = employeeInput.updateData();
+//			select = employeesDAO.updateData(employeesDTO);
+//			if(select > 0) {
+//				employeesView.view("수정에 성공하였습니다");
+//			}else {
+//				employeesView.view("수정에 실패하였습니다");
+//			}
+//			break;
+//		default:
+
 			default:
 				check=false;
-			}
-			
+		
+		
 		}
 		
 	}
+	
 }
+}
+
