2dc6bdbc7e2e5828a6d1762ecc0968229d935c32
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-24 21:47:34 +0900

    Update LocationDTO.java

--- src/main/java/com/iu/main/locations/LocationDTO.java
+++ src/main/java/com/iu/main/locations/LocationDTO.java
@@ -1,25 +1,12 @@
 package com.iu.main.locations;
 
 public class LocationDTO {
-	    //DTO만드는 규칙
-		//모든 멤버 변수의 접근 지정자는 private
-		//Table의 칼럼의 데이터타입과 컬럼명과 일치 시킴 
-		//Getter Setter 생성
-		//생성자는 여러개 있어도 상관 x
-		//꼭 기본 생성자(디폴트 생성자를 자동으로 만들어줌)는 있어야 함 
-		
-		//null값이 들어올 가능성이 있기때문에 integer(레퍼런스타입) 사용 
-		//변수명의 첫번째는 소문자로 시작하기 때문에 소문자로 작성 (컬럼명은 대소문자 구분 x)
-    
 	private Integer location_id;
 	private String street_address;
 	private String postal_code;
 	private String city;
 	private String state_province;
 	private String country_id;
-	
-	
-	
 	public Integer getLocation_id() {
 		return location_id;
 	}
@@ -57,8 +44,5 @@ public class LocationDTO {
 		this.country_id = country_id;
 	}
 	
-
-
-
-
 }
+
