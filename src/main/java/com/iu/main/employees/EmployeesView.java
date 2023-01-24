6a7a952dd3b6262db5500488c0ed80d17f8f4285
hee jin kim <121842612+jinjinzala@users.noreply.github.com>
2023-01-25 00:01:01 +0900

    Update EmployeesView.java

--- src/main/java/com/iu/main/employees/EmployeesView.java
+++ src/main/java/com/iu/main/employees/EmployeesView.java
@@ -11,9 +11,16 @@ public class EmployeesView {
 		System.out.println(employeesDTO.getEmployee_id());
 		System.out.println(employeesDTO.getFirst_name());
 		System.out.println(employeesDTO.getLast_name());
+		System.out.println(employeesDTO.getEamil());
+		System.out.println(employeesDTO.getPhone_number());
+		System.out.println(employeesDTO.getHire_date());
 		System.out.println(employeesDTO.getJob_id());
+		System.out.println(employeesDTO.getSalary());
+		System.out.println(employeesDTO.getCommision_pct());
+		System.out.println(employeesDTO.getManager_id());
 		System.out.println(employeesDTO.getDepartment_id());
 	}
+	
 	
 	public void view(ArrayList<EmployeesDTO> ar) {
 		for(EmployeesDTO employeesDTO : ar) {
